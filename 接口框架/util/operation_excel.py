9c8d110108244aa9126964617ec952ba1d81ae22
ZhuniYang <zhuni.yang@razer.com>
2019-06-28 17:28:52 +0800

    依赖：根据case_id找到对应的行号，根据行号返回内容

--- 接口框架/util/operation_excel.py
+++ 接口框架/util/operation_excel.py
@@ -2,6 +2,7 @@
 import xlrd
 from xlutils.copy import copy
 
+
 class OperationExcel:
     def __init__(self, file_name=None, sheet_id=None):
         if file_name:
@@ -14,30 +15,60 @@ class OperationExcel:
 
     # 获取sheets的内容
     def get_data(self):
-        data=xlrd.open_workbook(self.file_name)
+        data = xlrd.open_workbook(self.file_name)
         tables = data.sheets()[self.sheet_id]
         return tables
 
-    #获取单元格行数
+    # 获取单元格行数
     def get_lines(self):
         tables = self.data
         return tables.nrows
 
-    #获取单元格内容
-    def get_cell_value(self,row,col):
-        return self.data.cell_value(row,col)
+    # 获取单元格内容
+    def get_cell_value(self, row, col):
+        return self.data.cell_value(row, col)
 
-    #写入数据
-    def write_value(self,row,col,value):
-        read_data=xlrd.open_workbook(self.file_name)
-        write_data=copy(read_data)
-        sheet_data=write_data.get_sheet(0)
-        sheet_data.write(row,col,value)
+    # 写入数据
+    def write_value(self, row, col, value):
+        read_data = xlrd.open_workbook(self.file_name)
+        write_data = copy(read_data)
+        sheet_data = write_data.get_sheet(0)
+        sheet_data.write(row, col, value)
         write_data.save(self.file_name)
+
+    # 根据对应的caseid找到对应的行的内容
+    def get_rows_data(self, case_id):
+        row_num=self.get_row_num(case_id)
+        rows_data=self.get_rows_data(row_num)
+        return rows_data
+
+    # 根据对应的caseid找到对应的行号
+    def get_row_num(self, case_id):
+        num=0
+        cols_data=self.get_cols_data()
+        for col_data in cols_data:
+            if case_id in col_data:
+                return num
+            num=num+1
+        return num
+
+    # 根据行号，找到该行的内容
+    def get_row_values(self, row):
+        tables = self.data
+        row_data = tables.row_values(row)
+        return row_data
+
+    #获取某一列的内容
+    def get_cols_data(self,col_id=None):
+        if col_id!=None:
+            cols=self.data.col_values(col_id)
+        else:
+            self.data.col_values(0)
+        return cols
+
 
 
 
 if __name__ == '__main__':
     opers = OperationExcel()
-    print(opers.get_cell_value(2,3))
-
+    print(opers.get_cell_value(2, 3))
