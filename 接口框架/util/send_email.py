c117b34b8331cf522050086cdcf69ebb9785944f
ZhuniYang <zhuni.yang@razer.com>
2019-07-03 13:43:36 +0800

    发送邮件支持添加附件的功能

--- 接口框架/util/send_email.py
+++ 接口框架/util/send_email.py
@@ -1,6 +1,8 @@
 # coding:utf-8
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
 
 class SendEmail:
@@ -15,13 +17,22 @@ class SendEmail:
     to_receivers = ['meltest16@163.com', '409169403@qq.com']
     cc_receivers = ['meltest16@163.com']
 
-    def send_email(self,content):
+    def send_email(self, content):
         try:
-            msg = MIMEText(content, _subtype='plain', _charset='utf-8')
+            # 创建一个带附件的实例
+            msg = MIMEMultipart()
             msg['From'] = mail_user
             msg['To'] = ';'.join(to_receivers)
             msg['Cc'] = ';'.join(cc_receivers)
             msg['Subject'] = '接口自动化测试报告'
+            # 邮件正文内容
+            msg.attach(MIMEText(content, _subtype='plain', _charset='utf-8'))
+            # 构造附件
+            att1 = MIMEApplication(open("../dataconfig/case1.xls", 'rb').read())
+            att1["Content-Type"] = 'application/octet-stream'
+            att1.add_header('Content-Disposition', 'attachment', filename='接口自动化测试结果详情.xls')
+            msg.attach(att1)
+            # 邮件发送
             server = smtplib.SMTP(mail_host, 25)
             server.login(mail_user, mail_pass)
             server.sendmail(mail_user, msg['To'].split(';') + msg['Cc'].split(';'), msg.as_string())
@@ -36,11 +47,12 @@ class SendEmail:
         fail_num = len(fail_list)
         count_num = pass_num + fail_num
         pass_result = "%.2f%%" % (pass_num / count_num * 100)
-        fail_result="%.2f%%" % (fail_num / count_num * 100)
+        fail_result = "%.2f%%" % (fail_num / count_num * 100)
         content = "此次一共运行接口个数为%d个，通过个数为%d，失败个数为%d，通过率为%s,失败率为%s" % \
-                  (count_num, pass_num, fail_num, pass_result,fail_result)
+                  (count_num, pass_num, fail_num, pass_result, fail_result)
         self.send_email(content)
+
 
 if __name__ == '__main__':
     sen = SendEmail()
-    sen.send_main([1,2,3,4],[5,6,7])
+    sen.send_main([1, 2, 3, 4], [5, 6, 7])
